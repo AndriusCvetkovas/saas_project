e1cc2dfdc5d748efe627384e739b7e8d65daf9a0
AndriusCvetkovas <cvetkovas.andrius@gmail.com>
2020-02-18 17:18:47 -0800

    minor styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import './App.scss';
+import { findByLabelText } from '@testing-library/react';
 
 function App() {
   return (
-    <div className="Nav" style = {{width: '240px', height: '100vh', backgroundColor: 'white'}}>
+    <div className='main' style ={{display: 'flex', flexDirection: 'row'}}>
+      <div className="Nav" style = {{flex: 1, maxWidth: '240px', height: '100vh', backgroundColor: 'white'}}>
+      </div>
+      <div className='solution_container' style ={{flex: 5}}>
+          
+      </div>
     </div>
   );
 }
