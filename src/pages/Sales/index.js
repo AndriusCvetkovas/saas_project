7c49e4f1acdc4187e16d9fc462cf49042ee061e7
AndriusCvetkovas <cvetkovas.andrius@gmail.com>
2020-02-20 15:37:06 -0800

    header button search

--- src/pages/Sales/index.js
+++ src/pages/Sales/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 function SalesPage(){
     return(
-        <div>
-
+        <div style = {{marginLeft: '39px', marginTop: '39px', overflowY: 'auto'}}>
+            <Header 
+                title={'Sales/Orders'} 
+            />
+            <Button
+                type='shadowBlue'
+                wide={true}
+                text={'+ Add Orders'}
+            />
+            <Search
+                showplaceholder={true}
+                placeholder= 'Search orders with order#, customer information, SKU, etc.'
+                wide={true}
+                shadow={true}
+            /> 
         </div>
     )
 }
