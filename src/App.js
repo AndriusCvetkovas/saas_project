db32b77bb35af8898726e4e5e20c3054f02e75fa
AndriusCvetkovas <cvetkovas.andrius@gmail.com>
2020-02-23 17:47:41 -0800

    navbar

--- src/App.js
+++ src/App.js
@@ -10,6 +10,31 @@ function App() {
   return (
     <div className='main'>
       <div className="Nav">
+        <img></img>
+        <div className='company_name'>Inspiration Furniture</div>
+        <div>
+          <div className='dashboard_section wrap'>
+            <h1>Dashboard</h1>
+            <div>User</div>
+            <div>Profile Settings</div>
+            <div>Billing Settings</div>
+          </div>
+          <div className='sales_section wrap'>
+            <h1>Sales</h1>
+            <div>Orders</div>
+            <div>Returns</div>
+            <div>Invoice</div>
+            <div>Shipping</div>
+          </div>
+          <div className='inventory_section wrap'>
+            <h1>Inventory</h1>
+            <div>Overviews</div>
+            <div>Templates</div>
+          </div>
+          <div className='customer_section wrap'>
+            <h1>Customer</h1>
+          </div>
+        </div>
       </div>
       <div className='solution_container'>
           <SalesPage/>
