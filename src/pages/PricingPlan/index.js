90c58e8142801df7ecd54133b170a3fd90406f57
jackytam <jackytam2020@gmail.com>
2020-03-11 18:59:48 -0700

    added pricing page

---
+++ src/pages/PricingPlan/index.js
@@ -0,0 +1,54 @@
+import React,{useState} from 'react';
+import Header from '../../comps/Header';
+import { Link } from "react-router-dom";
+import PricingBox from '../../comps/PricingBoxes';
+//npm packages
+
+function PricingPage(){
+
+    return (
+        <div className="wrapper">
+            <div className="flexcenter">
+                <h1>Pricing & Plan</h1>
+                 <p>Unlock more features, generate more profit just at your fingertips, signup today!</p>
+            </div>
+
+            <div className="pricing-box-container">
+                <PricingBox 
+                    title={"Starter"}
+                    price={"19.99"}
+                    products={"150"}
+                    users={"6"}
+                    feature1={"Placing Orders"}
+                    feature2={"20GB storage"}
+                    feature3={"24/7 support"}
+                    buttonType={"starter"}
+                />
+                <PricingBox 
+                    title={"Professional"}
+                    price={"39.99"}
+                    products={"350"}
+                    users={"25"}
+                    feature1={"Template Features"}
+                    feature2={"50GB storage"}
+                    feature3={"24/7 support within 5 hours"}
+                    buttonType={"Professional"}
+                />
+                <PricingBox 
+                    title={"Enterprise"}
+                    price={"69.99"}
+                    products={"500"}
+                    users={"50"}
+                    feature1={"Placing Orders"}
+                    feature2={"1TB storage"}
+                    feature3={"24/7 support within 5 hours"}
+                />
+            </div>
+
+            <div style={{height:'150px'}}></div>
+           
+        </div>
+    )
+}
+
+export default PricingPage;
---
+++ src/stories/PricingBox.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.scss';
+
+import PricingBox from '../comps/PricingBoxes';
+
+export default {
+    title: "PricingBox",
+    component: PricingBox
+}
+export const DefaultPricingBox = () => {
+    return <PricingBox
+    />
+}
